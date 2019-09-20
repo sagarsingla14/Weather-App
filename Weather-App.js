ba3289c48722f5ec8e982b42784192b06a4f8299
github_sagarsingla14 <sagarrock1499@gmail.com>
2019-09-20 17:31:05 +0530

    Weather App Using Callback

--- Weather-App.js
+++ Weather-App.js
@@ -20,22 +20,33 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(results.address);
-    console.log(JSON.stringify(results , undefined , 3));
+    console.log('\nLocation ......\n');
+    console.log('Address : ' + results.address);
+    console.log('Latitude : ' + results.latitude);
+    console.log('Longitude : ' + results.longitude);
+
     weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
       var tempF = weatherResults.temperature;
       var appTempF = weatherResults.apparentTemperature;
+      var precipProbability = weatherResults.precipProbability;
+      var humidity = weatherResults.humidity;
+      var windSpeed = weatherResults.windSpeed;
 
       var tempC = ((tempF - 32) * 5) / 9;
       var appTempC = ((appTempF - 32) * 5) / 9;
+      console.log(' \nWeather ....... \n');
 
       if (errorMessage) {
         console.log(errorMessage);
       } else {
-        console.log(`Current Temperature F*: ${tempF}`);
-        console.log(`Feels like F*: ${appTempF}`);
-        console.log(`Current Temperature in C*: ${tempC}`);
-        console.log(`Feels like C*: ${appTempC}`);
+        console.log(`Current Temperature F* : ${tempF}`);
+        console.log(`Feels like F* : ${appTempF}`);
+        console.log(`Current Temperature in C* : ${tempC}`);
+        console.log(`Feels like C* : ${appTempC}`);
+
+        console.log(`precipProbability : ${precipProbability}`);
+        console.log(`Humidity : ${humidity}`);
+        console.log(`WindSpeed : ${windSpeed} m/s`);
       }
     });
   }
